c62d1f27f21051e3716f1d68dc92ac20ac27fd4c
victor-stephen <victorstephen047@gmail.com>
2025-06-07 04:32:06 +0100

    allow user guess and update keyboard based on guess

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { nanoid } from "nanoid";
 import { languages } from "./languages";
+import { generate } from "random-words";
 import LanguageChip from "./components/LanguageChip";
 import Header from "./components/Header";
 import WordChip from "./components/WordChip";
 import Button from "./components/Button";
 
 function App() {
-  const [word, setWord] = useState("react");
+  const [word, setWord] = useState(() => generate({ maxLength: 6 }));
+  const [guessedLetters, setGuessedLetters] = useState([]);
 
+  // Create the virtual keyboard
   const alphabets = "abcdefghijklmnopqrstuvwxyz";
-  const alphabetElementChips = alphabets.split('').map((alph) => (
-    <Button alphabet={alph.toUpperCase()} />
-  ));
+  const keyboardElement = alphabets.split("").map((alphabet) => {
+    const capitalLetter = alphabet.toUpperCase();
+    const wordCap = word.toUpperCase().split("");
+    const isGuessed = guessedLetters.includes(capitalLetter);
+    const isCorrectGuess = isGuessed && wordCap.includes(capitalLetter);
+    return (
+      <Button
+        key={capitalLetter}
+        addGuessedLetters={() => addGuessedLetters(capitalLetter)}
+        alphabet={capitalLetter}
+        guessedLetters={guessedLetters}
+        word={wordCap}
+        isGuessed={isGuessed}
+        isCorrectGuess={isCorrectGuess}
+      />
+    );
+  });
 
-  const wordElementChips = word.split("").map((letter) => (
-    <WordChip
-      key={nanoid()}
-      letter={letter.toUpperCase()}
-    />
-  ));
+  // Convert word individual letters into styled chips
+  const letterElement = word.split("").map((letter) => {
+    const capLetter = letter.toUpperCase();
+    return guessedLetters.includes(capLetter) ? (
+      <WordChip
+        key={nanoid()}
+        letter={capLetter}
+      />
+    ) : (
+      <WordChip
+        key={nanoid()}
+        letter="*"
+      />
+    );
+  });
+
+  // Render each programming language as a styled chip
   const languageElementChips = languages.map((lang) => (
     <LanguageChip
       key={nanoid()}
@@ -27,13 +55,32 @@ function App() {
     />
   ));
 
+  // Update user's letter choices when a key is clicked
+  function addGuessedLetters(letter) {
+    if (guessedLetters.length < 8)
+      return guessedLetters.includes(letter)
+        ? guessedLetters
+        : setGuessedLetters((prevGuess) => [...prevGuess, letter]);
+    
+    /* const guessSet = new Set(guessedLetters)  */
+  }
+
+  function startNewGame() {
+    setWord(generate());
+  }
+
   return (
     <main>
       <Header />
       <section className="languages">{languageElementChips}</section>
-      <section className="word">{wordElementChips}</section>
-      <section className="keyboard">{alphabetElementChips}</section>
-      <button className="new-game">New Game</button>
+      <section className="word">{letterElement}</section>
+      <section className="keyboard">{keyboardElement}</section>
+      <button
+        className="new-game"
+        onClick={startNewGame}
+      >
+        New Game
+      </button>
     </main>
   );
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
 
-export default function Button({alphabet}) {
+export default function Button(props) {
+  console.log(props.guessedLetters.length === 8);
+  const styles = {
+    background: (props.isCorrectGuess && "green") || (props.isGuessed && "red"),
+    opacity: props.guessedLetters.length === 8 && "0.5",
+  };
+
   return (
-    <button className='alphabet'>{alphabet}</button>
-  )
+    <button
+      className="alphabet"
+      onClick={props.addGuessedLetters}
+      style={styles}
+    >
+      {props.alphabet}
+    </button>
+  );
 }
